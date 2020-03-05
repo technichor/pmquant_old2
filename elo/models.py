6cb47c3a9742a65f27857fad0c49eb79f0ddeebd
technichor <corey.b.becker@gmail.com>
2020-03-05 13:50:23 -0500

    confirm github connection

---
+++ elo/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class idea(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.EmailField()
+    score = models.SmallIntegerField(default=1500)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    release = models.CharField(max_length=10, blank=True)
+    s_in_review = 0
+    s_open = 1
+    s_rejected = 2
+    s_completed = 3
+    s_choices = (
+        (s_in_review, _('In Review')),
+        (s_open, _('Open')),
+        (s_rejected, _('Rejected')),
+        (s_completed, _('Completed')),
+    )
+    status = models.PositiveSmallIntegerField(
+        choices=s_choices,
+        default=s_in_review,
+    )
+    def __str__(self):
+        return self.title
---
+++ pmquant/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pmquant project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pmquant.settings')
+
+application = get_asgi_application()
