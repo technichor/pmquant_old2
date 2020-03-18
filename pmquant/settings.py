60399000c61aaceddcf8d77f47d8876da3b27372
technichor <corey.b.becker@gmail.com>
2020-03-18 11:44:20 -0400

    troubleshooting build failures

--- pmquant/settings.py
+++ pmquant/settings.py
@@ -73,14 +73,36 @@ WSGI_APPLICATION = 'pmquant.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'pmquant',
+            'USER': 'technichor',
+            'PASSWORD': 'dowhat?duhawks!',
+            'HOST': '/cloudsql/pmquant:us-central1:pmquant',
+        }
     }
-}
-
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'pmquant',
+            'USER': 'technichor',
+            'PASSWORD': 'dowhat?duhawks!',
+        }
+    }
+# [END db_setup]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
